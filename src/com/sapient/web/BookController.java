08647f2251ddf2c64f51aa376c20a6ae7073d3cd
unknown <aver34@DELTAHITI35.sapient.com>
2014-10-17 13:13:49 +0530

    Semi-implementation of Controllers.

---
+++ src/com/sapient/web/BookController.java
@@ -0,0 +1,61 @@
+package com.sapient.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.sapient.dao.DaoI;
+import com.sapient.vo.Author;
+import com.sapient.vo.Book;
+import com.sapient.vo.Publisher;
+import com.sapient.vo.Store;
+
+@Controller
+public class BookController {
+	private DaoI dao;
+
+	@Autowired
+	@Qualifier("daoImpl")
+	public void setDao(DaoI dao) {
+		this.dao = dao;
+	}
+
+	@RequestMapping(value = "/viewbooks.htm", method = RequestMethod.GET)
+	public String ViewAllBooks(Model model) {
+		List<Book> book = dao.getAllBooks();
+		model.addAttribute("book", book);
+		List<Author> author = dao.getAllAuthors();
+		model.addAttribute("author", author);
+		List<Publisher> publisher = dao.getAllPublishers();
+		model.addAttribute("publisher", publisher);
+		return "AllBooks";
+
+	}
+
+	@RequestMapping(value = "/viewauthors.htm", method = RequestMethod.GET)
+	public String ViewAllAuthors(Model model) {
+		List<Author> author = dao.getAllAuthors();
+		model.addAttribute("author", author);
+		return "AllAuthors";
+	}
+
+	@RequestMapping(value = "/viewpublishers.htm", method = RequestMethod.GET)
+	public String ViewAllPublisher(Model model) {
+		List<Publisher> publisher = dao.getAllPublishers();
+		model.addAttribute("publisher", publisher);
+		return "AllPublishers";
+	}
+
+	@RequestMapping(value = "/viewstores.htm", method = RequestMethod.GET)
+	public String ViewAllStores(Model model) {
+		List<Store> store = dao.getAllStores();
+		model.addAttribute("store", store);
+		return "AllStores";
+	}
+
+}
